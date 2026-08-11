8d653eba07be8406a8e60c3250c0a120d4be551e
Hawmee <nomenaloicrandriann@gmail.com>
2026-08-12 00:35:18 +0300

    utils_coder.c: Done

--- utils_coder.c
+++ utils_coder.c
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 10:05:03 by varandri          #+#    #+#             */
-/*   Updated: 2026/07/28 15:11:55 by varandri         ###   ########.fr       */
+/*   Updated: 2026/08/12 00:32:42 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ t_dongle	*new_dongle(int cool_down)
 	t_dongle	*dongle;
 
 	dongle = (t_dongle *)malloc(sizeof(t_dongle));
-	dongle->cool_down = cool_down;
 	if (!dongle)
 		return (NULL);
+	dongle->cool_down = cool_down;
 	return (dongle);
 }
 
@@ -28,31 +28,47 @@ t_coder	*new_coder(int number)
 	t_coder		*coder;
 
 	coder = (t_coder *)malloc(sizeof(t_coder));
-	// coder->thread = NULL;
+	if (!coder)
+		return (NULL);
 	coder->number = number;
 	coder->compile_time = 0;
 	coder->debug_time = 0;
 	coder->refactor_time = 0;
 	coder->burnout_time = 0;
-	*(coder->dongle) = new_dongle(0);
+	coder->dongle = new_dongle(0);
 	coder->prev_coder = NULL;
 	coder->next_coder = NULL;
 	coder->is_last = 1;
-	if (!coder)
-		return (NULL);
 	return (coder);
 }
 
 t_coder	*last_coder(t_coder *coder)
 {
-	while (coder && !coder->is_last)
+	while (coder && coder->next_coder && !coder->is_last)
 		coder = coder->next_coder;
 	return (coder);
+}
+
+void	arrange_coders(t_coder *coder)
+{
+	t_coder	*first_coder;
+
+	if (!coder)
+		return ;
+	first_coder = coder;
+	first_coder->prev_coder = last_coder(coder);
+	while (coder && !coder->is_last)
+	{
+		(coder->next_coder)->prev_coder = coder;
+		coder = coder->next_coder;
+	}
+	coder->next_coder = first_coder;
 }
 
 t_coder	*create_coders(int numbers)
 {
 	t_coder		*coder;
+	t_coder		*tail;
 	int			i;
 
 	i = 1;
@@ -65,12 +81,11 @@ t_coder	*create_coders(int numbers)
 			i++;
 			continue ;
 		}
-		last_coder(coder)->next_coder = new_coder(i);
-		(last_coder(coder)->next_coder)->prev_coder = last_coder(coder);
-		last_coder(coder)->is_last = 0;
+		tail = last_coder(coder);
+		tail->is_last = 0;
+		tail->next_coder = new_coder(i);
 		i++;
 	}
-	last_coder(coder)->next_coder = coder;
-	coder->prev_coder = last_coder(coder);
+	arrange_coders(coder);
 	return (coder);
 }
