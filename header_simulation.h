76e71acf8d1c5f627127010f86ccc94a503e54a2
Hawmee <nomenaloicrandriann@gmail.com>
2026-08-17 23:20:09 +0300

    [feature] simulation: added

--- header_simulation.h
+++ header_simulation.h
@@ -6,12 +6,19 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 13:19:41 by varandri          #+#    #+#             */
-/*   Updated: 2026/08/17 22:13:40 by varandri         ###   ########.fr       */
+/*   Updated: 2026/08/17 23:03:28 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_SIMULATION_H
 # define HEADER_SIMULATION_H
 # include "header_coder.h"
+
+typedef	struct s_arg
+{
+	t_config	*config;
+	t_coder		*coder;
+}	t_arg;
+
 
 #endif
